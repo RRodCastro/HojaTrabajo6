132f432cc59ed4333815d6a4f84af0e5588e9d1d
RRodCastro <mrcastro9574@gmail.com>
2015-09-01 19:44:15 -0600

    Create TiposDeSet.java

---
+++ TiposDeSet.java
@@ -0,0 +1,65 @@
+//Clase Padre
+
+import java.util.HashSet;
+
+
+public abstract class TiposDeSet {
+	
+	Desarollador d;
+
+	public Desarollador getD() {
+		return d;
+	}
+
+	public void setD(Desarollador d) {
+		this.d = d;
+	}
+	
+	
+	
+	public  void Agregar(){
+		
+	}
+	
+	
+	
+	
+	// Conjunto con Experiencia en Java, Web, Celulares
+	public void Conjunto1(){
+		
+		
+		
+	}
+	
+	//Conjunto con Experiencia en Java pero sin experiencia Web
+	
+	public void Conjunto2(){
+		
+	}
+	//Experiencia Web y celulares, sin experiencia Java
+	
+	public void Conjunto3(){
+		
+	}
+	
+	//Experiencia Web o celulares sin experiencia Java
+	public void Conjunto4(){
+		
+	}
+	
+	public void Conjunto5(){
+		
+	}
+	
+	public void conjuntoMayor(){
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
